580d46bdb07ba2653acec0d20260d2585097b991
Mahdi Zanganeh <mahdizedn9ne@gmail.com>
2023-10-11 01:32:34 +0330

    Project has been finished

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const h5 = document.getElementById('h5');
+const homePage = document.querySelector('.home-page');
+const successMessage = document.querySelector('.success-container');
+const Return = document.getElementById('return');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    
+    var value = input.value;
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (value.match(validRegex)) {
+        h5.classList.remove('h5');
+        input.classList.remove('not-valid');
+        homePage.classList.add('home-off');
+        successMessage.classList.add('success-on');
+        return true;
+    } else {
+        h5.classList.add('h5');
+        input.classList.add('not-valid');
+        return false;
+    }
+});
+Return.addEventListener('click', function () {
+    displayReset();
+})
+function displayReset() {
+    h5.classList.remove('h5');
+    input.classList.remove('not-valid');
+    homePage.classList.remove('home-off');
+    successMessage.classList.remove('success-on');
+    if (input.value) {
+        input.value = '';
+    }
+}
